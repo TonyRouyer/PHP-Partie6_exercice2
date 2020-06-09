1b283846a94f133391aec177535bd3d18c3398d8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 13:47:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 6 exercice 2</title>
+    </head>
+    <body>
+        <a href="index.php?lastname=Nemare&amp;firstname=Jean&amp;activate=true">Clique ici !</a>
+        <?php 
+            if ( isset($_GET['activate'])){
+                //si firstname et lastname sont defini -> les recupère et affiche le echo
+                if (isset($_GET['firstname']) && isset($_GET['lastname']) && (isset($_GET['age']))) {
+                    echo '<p>' . $_GET['firstname'] . ' ' . $_GET['lastname'] . ' est bien présent !</p>';
+                }else {
+                    echo '<p>Un des paramètre n\'est pas defini</p>';
+                }
+            }
+        ?>
+    </body>
+</html>
